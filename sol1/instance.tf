fe7ee2ce85526e3f69ebeb93370083e041d476e4
lazyTitan157 <1azytitan157@gmail.com>
2020-05-12 19:39:48 +0900

    set

--- sol1/instance.tf
+++ sol1/instance.tf
@@ -1,15 +1,13 @@
 resource "aws_key_pair" "kt-cloud-key" {
   key_name   = "kt-cloud-key"
-  public_key = "~/mykey.pub"
-  #file(var.PATH_TO_PUBLIC_KEY)
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-00edfb46b107f643c"
   #lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  key_name      = "kt-cloud-key"
-  #aws_key_pair.kt-cloud-key.key_name
+  key_name      = aws_key_pair.kt-cloud-key.key_name
 
   provisioner "file" {
     source      = "script.sh"
@@ -29,8 +27,7 @@ resource "aws_instance" "example" {
     host        = coalesce(self.public_ip, self.private_ip)
     user        = "ubuntu"
     #var.INSTANCE_USERNAME
-    private_key = "~/mykey"
-    #file(var.PATH_TO_PRIVATE_KEY)
+    private_key = file(var.PATH_TO_PRIVATE_KEY)
   }
   tags = {
   }
