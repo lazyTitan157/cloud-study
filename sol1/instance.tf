3d6f8265ca2a2fba84bb68b415cb5584ec5c6167
lazyTitan157 <1azytitan157@gmail.com>
2020-05-13 09:38:14 +0900

    key setting

--- sol1/instance.tf
+++ sol1/instance.tf
@@ -1,12 +1,12 @@
-resource "aws_key_pair" "kt-cloud-key" {
-  key_name   = "kt-cloud-key"
+resource "aws_key_pair" "mykey" {
+  key_name   = "mykey"
   public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
 resource "aws_instance" "example" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.kt-cloud-key.key_name
+  key_name      = aws_key_pair.mykey.key_name
 
   provisioner "file" {
     source      = "script.sh"
