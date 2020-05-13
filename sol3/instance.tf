8d384f93c880aa40df745dd318c11b66ac81fd25
lazyTitan157 <1azytitan157@gmail.com>
2020-05-13 19:33:22 +0900

    sol3: nginx install script added

--- sol3/instance.tf
+++ sol3/instance.tf
@@ -10,6 +10,23 @@ resource "aws_instance" "example1" {
 
   # the public SSH key
   key_name = aws_key_pair.ktcloudkey.key_name
+
+  provisioner "file" {
+    source      = "script.sh"
+    destination = "/tmp/script.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "sudo /tmp/script.sh"
+    ]
+  }
+
+  connection {
+    host        = coalesce(self.public_ip, self.private_ip)
+    user        = var.INSTANCE_USERNAME
+    private_key = file(var.PATH_TO_PRIVATE_KEY)
+  }
 }
 
 resource "aws_instance" "example2" {
@@ -24,6 +41,23 @@ resource "aws_instance" "example2" {
 
   # the public SSH key
   key_name = aws_key_pair.ktcloudkey.key_name
+
+  provisioner "file" {
+    source      = "script.sh"
+    destination = "/tmp/script.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "sudo /tmp/script.sh"
+    ]
+  }
+
+  connection {
+    host        = coalesce(self.public_ip, self.private_ip)
+    user        = var.INSTANCE_USERNAME
+    private_key = file(var.PATH_TO_PRIVATE_KEY)
+  }
 }
 
 resource "aws_instance" "example3" {
@@ -38,4 +72,21 @@ resource "aws_instance" "example3" {
 
   # the public SSH key
   key_name = aws_key_pair.ktcloudkey.key_name
+
+  provisioner "file" {
+    source      = "script.sh"
+    destination = "/tmp/script.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "sudo /tmp/script.sh"
+    ]
+  }
+
+  connection {
+    host        = coalesce(self.public_ip, self.private_ip)
+    user        = var.INSTANCE_USERNAME
+    private_key = file(var.PATH_TO_PRIVATE_KEY)
+  }
 }
